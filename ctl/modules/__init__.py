b3d9833288720a7459f854e4c8e247f3562148c4
Jim Neuendorf <jimneuendorf@gmx.de>
2017-01-12 20:07:27 +0100

    removed old code belonging to old dynamic module access

--- ctl/modules/__init__.py
+++ ctl/modules/__init__.py
@@ -5,15 +5,3 @@ from .homebrew import homebrew # NOQA
 from .mysql import mysql # NOQA
 from .postgres import postgres # NOQA
 from .rails import rails # NOQA
-
-# make all modules accessible by name
-# key should match module_class.name (this is what the user has to type)
-module_dict = {
-    "abstract_project": abstract_project,
-    "atom": atom,
-    "git": git,
-    "homebrew": homebrew,
-    "mysql": mysql,
-    "postgres": postgres,
-    "rails": rails,
-}
